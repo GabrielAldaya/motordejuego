1e6c651ed0053d54994a4051f7ad293b49214efe
nicoromero.22.03@gmail.com <nicoromero.22.03@gmail.com@5a76464c-81c4-c038-8244-9f079e46f001>
2012-04-26 01:20:42 +0000

    Funciona la ventana. No compila por agregados.

--- trunk/ motordejuego --username nicoromero.22.03@gmail.com/Engine/Renderer.h
+++ trunk/ motordejuego --username nicoromero.22.03@gmail.com/Engine/Renderer.h
@@ -3,14 +3,15 @@
 #include<D3D9.h>
 #include "VertexBuffer.h"
 #include "Structs.h"
+#include "Defines.h"
 #pragma comment (lib, "d3d9.lib")
 #pragma comment (lib, "d3dx9.lib")
-class Renderer
+class ENGINE_API Renderer
 {
 public:
-	Renderer();
+	Renderer(HWND hWnd);
 	~Renderer();
-	bool InitDX(HWND hWnd);
+	bool InitDX(HWND hWnd, bool bisWindowed);
 	void StartFrame();
 	void EndFrame();
 	enum PrimitiveType
@@ -21,6 +22,11 @@ public:
 		LINE_STRIP = D3DPT_LINESTRIP
 	};
 	void Draw(ColorVertex * vertexColletion, PrimitiveType prim, unsigned int uiVertexCount);
+	void translate(float fX, float fY, float fZ = 0.0f);
+	void setMatrixMode(MatrixMode modo);
+	void RotateZ(float fAngle);
+	void Scale(float fW, float fH);
+
 private:
 	IDirect3DDevice9* m_pkDevice;
 
